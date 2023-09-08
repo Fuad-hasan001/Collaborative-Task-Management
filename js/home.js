1d58bb1e72fcfc9ced9b6c98c7c58799560be41f
Fuad-hasan001 <spotlight.neutron0@gmail.com>
2023-09-08 20:15:23 +0600

    updating main page

--- js/home.js
+++ js/home.js
@@ -1,7 +1,14 @@
-const homePage = () =>{
+const mainPage = () =>{
     window.location.href = "http://127.0.0.1:5500/mainPage/main.html"
+
+    // Authentication part
+
+
 }
 
 const signUpPage = () =>{
     window.location.href = "http://127.0.0.1:5500/signup.html"
+
+    // Authentication part
+
 }
