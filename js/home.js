caf6b646172fd41b4ff8226ce12e8aa5d5a6c796
Fuad-hasan001 <spotlight.neutron0@gmail.com>
2023-09-08 19:50:00 +0600

    updating Registration page with js file

--- js/home.js
+++ js/home.js
@@ -1,3 +1,7 @@
 const homePage = () =>{
-    window.location.href = "main.html"
+    window.location.href = "http://127.0.0.1:5500/mainPage/main.html"
+}
+
+const signUpPage = () =>{
+    window.location.href = "http://127.0.0.1:5500/signup.html"
 }
